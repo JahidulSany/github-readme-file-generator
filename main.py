bf78b86caad1e85543095af5731aefd50490ae05
JahidulSany <jahidulsanypro@gmail.com>
2026-02-14 21:30:29 +0000

    Content Data for Github Readme file added

--- main.py
+++ main.py
@@ -29,6 +29,40 @@ class GithubReadmeGenerator:
         self.contact = inquirer.text(message="Contact: ").execute()
 
 
+        # Generating Content Data
+    def generate_content_data(self):
+        content = f"""
+### 🌐 {self.title}
+
+# ✨ Project Description
+
+{self.desc}
+
+# 🛠️ Installation Instructions
+
+{self.install_instruct}
+
+# 💥 Usage Instructions
+
+{self.usage_instruct}
+
+# 🪪 License Options
+
+{self.license}
+
+# ✍️ Author:
+
+{self.author}
+
+# 📇 Contact:
+
+{self.contact}
+
+👏 Thanks for checking out! I hope it gives you a strong sense of my skills and capabilities.
+
+"""    
+        return content
+
 
 
 
